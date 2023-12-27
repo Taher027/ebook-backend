ebbf83535552c673282aef93310470070f746a19
Abu Taher Hossain <abutaherh27@gmail.com>
2023-12-27 18:28:33 +0600

    make get all review fuction service and complte review module

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -6,3 +6,13 @@ const addReview = async (payload: IReview): Promise<IReview | null> => {
     const result = await Review.create(payload);
     return result;
   };
+
+  const getAllReviews = async (bookId:string): Promise<IReview[] | null> => {
+    const result = await Review.find({ bookId }).sort({ createdAt: -1 });
+    return result;
+  };
+
+export const ReviewService = {
+    addReview,
+    getAllReviews
+}
