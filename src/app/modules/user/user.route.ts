fc372f424cd6582786ed7315359f75303b930f0f
Abu Taher Hossain <abutaherh27@gmail.com>
2023-12-27 13:59:29 +0600

    complte user module

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,12 @@
 import { UserController } from "./user.controller";
 import express from 'express'
 const router = express.Router()
+// update user
+router.patch('/:id',
+  UserController.updateUser);
 
+// delete user
+router.delete('/:id', UserController.deleteUser);
 router.get('/:id/wishlist', UserController.getUserWishlist)
 router.get('/:id/readinglist', UserController.getUserReadingList)
 
